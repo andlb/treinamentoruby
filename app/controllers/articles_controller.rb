d473af573f26f308586db8223bf157ce1b5fda45
andre <andlbp@gmail.com>
2017-05-25 12:09:15 +0000

    adicionando, editando, mostrando e index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def new
         @article = Article.new
     end
@@ -16,10 +20,25 @@ class ArticlesController < ApplicationController
         #redirect_to articles_show(@article)
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'article foi atulizado com sucesso '
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @article = Article.find(params[:id])
         
     end
+    
     private
     def article_params 
         params.require(:article).permit(:title,:description)
